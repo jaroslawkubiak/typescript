a53a20fce3b2a7a5ba1f13a8dd166aa354d994a3
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2023-09-07 22:44:49 +0200

    finish section 8

---
+++ features/classes.ts
@@ -0,0 +1,26 @@
+class Vehicle {
+  constructor(public color: string) {}
+  protected honk(): void {
+    console.log("honk");
+  }
+}
+
+const vehicle = new Vehicle("white pearl");
+console.log(vehicle.color);
+
+class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+  private drive(): void {
+    console.log("drive drive");
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+  }
+}
+
+const car = new Car(5, "miami blue");
+car.startDrivingProcess();
+console.log(car.color);
