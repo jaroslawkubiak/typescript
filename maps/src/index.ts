fb42c130e3b9a11161a2788b65357dae04b08a41
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2023-09-10 14:52:02 +0200

    section 9, part 1

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,18 +1,12 @@
 /// <reference types="@types/google.maps" />
 
-// import { User } from "./User";
-// import { Company } from "./Company";
+import { User } from "./User";
+import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 
-// const user = new User();
-// console.log(user);
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap("map");
 
-// const company = new Company();
-// console.log(company);
-
-new google.maps.Map(document.getElementById("map") as HTMLElement, {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+customMap.addMarker(user);
+customMap.addMarker(company);
