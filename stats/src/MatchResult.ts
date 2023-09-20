4af57fa68f69d5fb033e1c855b2de12528cb3247
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2023-09-20 21:51:58 +0200

    section 11 - creating csvfilereader class

---
+++ stats/src/MatchResult.ts
@@ -0,0 +1,9 @@
+// typescritp approach - enum - enumeration. enum stores very closely related values
+// enum to sygnał dla innych inżynierów, że zawiera on ściśle powiązane wartości
+// enum tworzy także TYP
+export enum MatchResult {
+    HomeWin = "H",
+    AwayWin = "A",
+    Draw = "D",
+  }
+  
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,17 +1,38 @@
-import fs from "fs";
+import { CvsFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult";
 
-const matches = fs
-  .readFileSync("football.csv", { encoding: "utf-8" })
-  .split("\n")
-  .map((row: string): string[] => {
-    return row.split(",");
-  });
+const reader = new CvsFileReader("football.csv");
+reader.read();
+
+// import fs from "fs";
+// const matches = fs
+//   .readFileSync("football.csv", { encoding: "utf-8" })
+//   .split("\n")
+//   .map((row: string): string[] => {
+//     return row.split(",");
+//   });
+
+// better than comparison with plain string "H", but not so good
+// const HOME_WIN = "H";
+// const AWAY_WIN = "A";
+
+// almost good, but why it is an object?
+// const MatchResult = {
+//   HomeWin: "H",
+//   AwayWin: "A",
+//   Draw: "D",
+// };
 
 let manUnitedWins = 0;
 
-matches.filter((row: string[]): void => {
-  if ((row[1] === "Man United" && row[5] === "H") || (row[2] === "Man United" && row[5] === "A"))
+for (let match of reader.data) {
+  if (
+    (match[1] === "Man United" && match[5] === MatchResult.HomeWin) ||
+    (match[2] === "Man United" && match[5] === MatchResult.AwayWin)
+  )
     manUnitedWins++;
-});
+}
 
 console.log(`Man United won ${manUnitedWins} games`);
+
+const test = reader.data[0][1];
